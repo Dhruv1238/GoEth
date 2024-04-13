6c000e0c8f60007fd19623b9829509030537176a
Dhruv1238 <dhruv4075@gmail.com>
2024-04-13 20:13:37 +0530

    fix: PfP

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -38,10 +38,7 @@ function Navbar() {
           </Link>
           <Link to="/profile">
             <button type="button" className="inline-flex flex-col items-center justify-center px-5 group">
-              <img className="w-8 h-8 mb-0 rounded-full text-gray-900   group-hover:text-blue-500" src={user!=null ? user.photoURL :
-                <svg className="w-5 h-5 mb-2 text-gray-900   group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                </svg>} alt="" />
+              <img className="w-8 h-8 mb-0 rounded-full text-gray-900   group-hover:text-blue-500" src={user.photoURL} alt="" />
               <span className="text-sm text-gray-900   group-hover:text-blue-500">Profile</span>
             </button>
           </Link>
