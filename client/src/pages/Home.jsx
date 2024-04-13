5afd467a7a49677dc779b0d8854414ea5c1b5a69
neilo <neil.carnac@somaiya.edu>
2024-04-14 04:49:32 +0530

    feat:ui changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -220,13 +220,16 @@ function Home() {
 
                     <div className='flex flex-col h-full justify-between overflow-hidden p-8  '>
                         <div className='flex flex-col gap-4'>
-                            <div className='flex flex-row justify-between'>
+                            <div className='flex flex-row justify-between z-10'>
                                 <Button className='flex items-center justify-center w-10 h-10 bg-black rounded-lg'>
-                                    
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 text-white h-6">
+                                        <path fillRule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+                                    </svg>
+
                                 </Button>
                                 <div className='flex flex-row gap-3'>
                                     <Button className='flex items-center justify-center w-10 h-10 bg-black rounded-lg'>
-                                        
+
                                     </Button>
                                     <Button className='flex items-center justify-center w-10 h-10 bg-black rounded-lg'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 transform rotate-12">
