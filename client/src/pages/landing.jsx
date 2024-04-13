f9e890b4e9f1d1961160772cdecf2a4a5ffd5f83
Dhruv1238 <dhruv4075@gmail.com>
2024-04-13 11:05:45 +0530

    feat: Tailwind CSS

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {Button} from "@material-tailwind/react";
 
 const Landing = () => {
     return (
         <>
             <div>
                 <h2 className="text-white ">hellow</h2>
+                <Button color="purple" >
+                    Hello World
+                </Button>
             </div>
         </>
     );
