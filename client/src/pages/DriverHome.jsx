3ee1eca4306cd64804971ae63575e167d5d4dfee
neilo <neil.carnac@somaiya.edu>
2024-04-14 02:06:17 +0530

    feat: small uichanges

--- client/src/pages/DriverHome.jsx
+++ client/src/pages/DriverHome.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+import { Button } from '@material-tailwind/react'
 
 function DriverHome() {
+    const [distance, setDistance] = useState('13')
+    const [destination, setDestination] = useState('KJ Somaiya')
+    const [estimatedMinCost, setEstimatedMinCost] = useState('156');
+    const [estimatedMaxCost, setEstimatedMaxCost] = useState('156');
+
     return (
         <>
             <div className='w-[45vh] h-[100vh] bg-white relative'>
+                <div className="flex flex-col pl-8 pr-8 pt-8 gap-2">
+                    <div className='flex flex-col gap-1 p-4'>
 
+                        <p className='text-2xl leading-tight font-medium'>Rides Available <span className=' text-green-600'>Live</span></p>
+                        <p className='text-gray-500 leading-tight'>4 Rides Found</p>
+                    </div>
+
+
+                    
+
+                </div>
             </div>
         </>
     )
