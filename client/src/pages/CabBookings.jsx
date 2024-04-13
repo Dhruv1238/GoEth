18307077e919d9f9881c16b70e869f513b67a5b1
neilo <neil.carnac@somaiya.edu>
2024-04-14 03:16:16 +0530

    feat: added booking page ui

--- client/src/pages/CabBookings.jsx
+++ client/src/pages/CabBookings.jsx
@@ -93,14 +93,23 @@ function CabBookings() {
                                             <img src="" alt="" />
                                         </div>
                                     </div>
-                                    <div className='flex flex-row text-xs gap-2 items-center'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                                        </svg>
+                                    <div className='flex flex-row justify-between items-center'>
+                                        <div className='flex flex-row text-xs gap-2 items-center'>
 
-                                        <p className='text-gray-700'>{car.timeToArrive}</p>
-                                        <p className='text-gray-700'>{car.distance}</p>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                            </svg>
+
+                                            <p className='text-gray-700'>{car.timeToArrive}</p>
+                                            <p className='text-gray-700'>{car.distance}</p>
+                                        </div>
+                                        {car.isElectric ? (
+                                            <div className='text-base text-green-400'>
+                                                <p className='font-bold'>Electric Car</p>
+                                            </div>
+                                        ) : null}
+
                                     </div>
                                     <div className="flex flex-row ">
 
