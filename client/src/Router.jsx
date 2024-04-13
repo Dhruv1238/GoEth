bdb8ea45ccb7736b0304b7ca6d56c060063a4c55
Pratham Goenka <prathamg1803@gmail.com>
2024-04-13 11:14:27 +0530

    Added Maps Page

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing';
+import Map from './pages/Map';
 
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Landing />} />
-
+            <Route path="/map" element={<Map />} />
         </Routes>
     );
 };
---
+++ client/src/pages/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <>
+            <h2 className="text-white ">Maps</h2>
+        </>
+    );
+};
+
+export default Map;
